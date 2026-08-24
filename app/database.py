90316418f0929a50acfa311755a2979c8fca5d59
HDem <93908678+HDem3@users.noreply.github.com>
2026-08-24 20:16:55 +0200

    add get_db()

---
+++ app/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine #collegamento tra sqlalchemy e il database
+from sqlalchemy.orm import sessionmaker #tipo il connect di sqlite3
+from sqlalchemy.orm import declarative_base #tipo il basemodel di pydantic
+from dotenv import load_dotenv
+import os
+
+load_dotenv() #carica le variabili d'ambiente dal file .env
+
+DATABASE_URL = os.getenv("DATABASE_URL") #prende la variabile d'ambiente DATABASE_URL dal file .env
+
+engine = create_engine(DATABASE_URL) #crea il collegamento tra sqlalchemy e il database
+
+SessionLocal= sessionmaker(
+    bind=engine, #collega la sessione al database
+    autocommit=False, #non fa il commit automatico
+    autoflush=False, #non fa il flush automatico
+)
+
+Base = declarative_base()
+
+def get_db():
+    db= SessionLocal()
+
+    try: yield db
+    finally: db.close()
+
