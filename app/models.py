359f17e17ac896b059273ad6e6b199871c8548dd
HDem <93908678+HDem3@users.noreply.github.com>
2026-08-25 12:29:13 +0200

    create products table with python

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float
+
+from app.database import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    sku = Column(String, unique=True, nullable=False)
+    quantity = Column(Integer, default=0)
+    price = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
