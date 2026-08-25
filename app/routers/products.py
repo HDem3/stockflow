cc3f42e87656d317125946811d47e0762bcbaf0d
HDem <93908678+HDem3@users.noreply.github.com>
2026-08-25 19:06:30 +0200

    fatto mini test e sistemato un import

--- app/routers/products.py
+++ app/routers/products.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app import models, schemas
-from app.database import engine, get_db
+from app.database import get_db
+
 
 router= APIRouter(
     prefix="/products", 
