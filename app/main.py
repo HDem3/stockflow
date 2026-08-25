08dddaa744f5551cd67adc0144560e456ecef956
HDem <93908678+HDem3@users.noreply.github.com>
2026-08-25 16:01:59 +0200

    create endpoint for delete products

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
-from app import models
-from app.database import engine
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+from app.database import engine, get_db
+
 
 app= FastAPI()
 
@@ -20,3 +23,66 @@ async def info():
     return {"name": "StockFlow", 
             "version": "1.0.0",
             "description": "Warehouse management API"}
+
+@app.post("/products", response_model=models.ProductResponse)
+async def create_product(product: schemas.ProductCreate, 
+                         db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=product.name,
+        sku=product.sku,
+        description=product.description,
+        price=product.price,
+        quantity=product.quantity
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return {"message": "Product created successfully"}
+
+@app.get("/products", response_model=list[schemas.ProductResponse])
+def get_products(db: Session = Depends(get_db)):
+    products = db.query(models.Product).all()
+
+    return products
+
+@app.get("/products/{product_id}", response_model=schemas.ProductResponse)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return product
+
+@app.put("/products/{product_id}", response_model=schemas.ProductResponse)
+def update_product(product_id: int, updated_product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    product.name = updated_product.name
+    product.sku = updated_product.sku
+    product.description = updated_product.description
+    product.price = updated_product.price
+    product.quantity = updated_product.quantity
+
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    db.delete(product)
+    db.commit()
+
+    return {"message": "Product deleted successfully"}
+
